2ab722356dd0a97a37d9a1fd69ee346e6c10a22a
ahmed-idenbrid <84811071+ahmed-idenbrid@users.noreply.github.com>
2022-03-08 15:22:46 +0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".topSlide", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+function makeActive() {
+	var element = document.getElementById("bg-active-sell");
+		element.classList.toggle("bg-active-sell");
+}
+function makeActivesell() {
+	var element = document.getElementById("buy-confirmation");
+		element.classList.toggle("bg-active-buy");
+}
+function OmitDetail(e){
+	console.log(e.target.nextSibling.innerHTML);
+}
