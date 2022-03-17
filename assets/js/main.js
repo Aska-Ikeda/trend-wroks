2b2be7b622a1d53cf088af50ff8461ed2ee1a61c
ahmed-idenbrid <84811071+ahmed-idenbrid@users.noreply.github.com>
2022-03-17 21:23:59 +0500

    active & deactive components done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,15 @@ function makeActive() {
 		element.classList.remove("bg-active-buy");
 	}
 		var element = document.getElementById("bg-active-sell");
-			element.classList.toggle("bg-active-sell");
+			element.classList.add("bg-active-sell");
 }
 function makeActivesell() {
 	if($("#bg-active-sell").hasClass("bg-active-sell")){
 		var element = document.getElementById("bg-active-sell");
-		element.classList.toggle("bg-active-sell");
+		element.classList.remove("bg-active-sell");
 	}
 		var element = document.getElementById("buy-confirmation");
-		element.classList.toggle("bg-active-buy");
+		element.classList.add("bg-active-buy");
 }
 function OmitDetail(e){
 	if(e.target.checked){
