302adadb8d1a56060aa30a9bdb97a4a48729943c
ahmed-idenbrid <84811071+ahmed-idenbrid@users.noreply.github.com>
2022-03-21 11:57:17 +0500

    checkbox btn js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,50 @@ var swiper = new Swiper(".topSlide", {
 		prevEl: ".swiper-button-prev",
 	},
 });
+
 function makeActive() {
-	if($("#buy-confirmation").hasClass("bg-active-buy")){
+	if ($("#buy-confirmation").hasClass("bg-active-buy")) {
 		var element = document.getElementById("buy-confirmation");
 		element.classList.remove("bg-active-buy");
 	}
-		var element = document.getElementById("bg-active-sell");
-			element.classList.add("bg-active-sell");
+	var element = document.getElementById("bg-active-sell");
+	element.classList.add("bg-active-sell");
 }
+
 function makeActivesell() {
-	if($("#bg-active-sell").hasClass("bg-active-sell")){
+	if ($("#bg-active-sell").hasClass("bg-active-sell")) {
 		var element = document.getElementById("bg-active-sell");
 		element.classList.remove("bg-active-sell");
 	}
-		var element = document.getElementById("buy-confirmation");
-		element.classList.add("bg-active-buy");
+	var element = document.getElementById("buy-confirmation");
+	element.classList.add("bg-active-buy");
 }
-function OmitDetail(e){
-	if(e.target.checked){
-		document.getElementById("")
+
+
+
+function OmitDetail(e) {
+	if (e.target.checked) {
+		$("#btn-sell-section").html(`<button type="button"
+		class="buy-confirmation-btn font-24 f-bold">買
+		い</button>`)
+	}
+	if (e.target.checked == false) {
+		$("#btn-sell-section").html(`<button type="button"
+		class="sell-confirmation-btn font-24 f-bold">
+		売り確認
+		</button>`)
+	}
+}
+function BuyOmitDetail(e) {
+	if (e.target.checked) {
+		$("#btn-buy-section").html(`<button type="button"
+		class="buy-confirmation-btn font-24 f-bold">買
+		い</button>`)
+	}
+	if (e.target.checked == false) {
+		$("#btn-buy-section").html(`<button type="button"
+		class="sell-confirmation-btn font-24 f-bold">
+		売り確認
+		</button>`)
 	}
 }
